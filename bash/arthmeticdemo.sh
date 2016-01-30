d1cf4d6d87169ed2bd128630053f5b939a2a7432
root <root@localhost.localdomain>
2016-01-30 01:27:01 +0000

    adding lab 1

---
+++ bash/arthmeticdemo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#this script demonstartes arithmetic
+
+#get two num from the user to use
+read -p "Give me a number " firstsum
+read -p "Give me another number " secondsum
+
+
+
+
+
+
+
+sum=$(( $firstnum + $secondnum ))
+echo "$firstnum plus $secondnum equals $sum"
+
+
+
+leftover=$(( $firstnum -$secondnum ))
+echo "$firstnum minus $secondnum equals $sum"
+
+
+
+
+product=$ (($
