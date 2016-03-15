b792fd8f3184f0cc7e52edd42a382268c28685d4
root <root@localhost.localdomain>
2016-03-15 21:22:29 +0000

    done picstat and interfaces

--- bash/lab4/picstats.sh
+++ bash/lab4/picstats.sh
@@ -1,11 +1,67 @@
 #!/bin/bash
-# this script displays how many files are in ~/Pictures,
-# how much space they use,
-# and the sizes and names of the 3 largest files
-echo -n There are `find ~/Pictures -type f | wc -l`
-echo " files in the ~/Pictures directory"
-echo "The Pictures directory uses:"
-du -sh ~/Pictures
-echo "The 3 largest files in the ~/Pictures directory are:"
-cd ~/Pictures
-find . -type f -exec du -h {} \; | sort -h | tail -3
+# this script will show how many files are in given directory,
+# how much MB or Kilobytes they are of, names of the 3 largest files
+
+#default directory
+dir=~/Pictures
+
+#defult number
+default=2
+
+
+#help function for the client
+help_function() {
+
+
+	cat <<-EOF
+		This  will count the number of pictures in Directory
+		And size of the Directory and also name of file
+		
+
+	Option Arguments
+	-c and --count (count the Number of files)
+
+	Example:
+		./filename -c 2  # print the 2 largest files in the given directory in command line
+
+	EOF
+}
+
+
+#error message-function
+error-message () {
+	echo "error message $filename: $1 " >&2
+}
+while [ $# -gt 0 ]; do
+	case "$1" in
+	-h | --help ) # to execute help function
+		help_function
+	exit 0
+	;;
+	-c | --count )
+		default=$2
+		shift
+		;;
+	* )
+		dir=~/$1
+	;;
+	esac
+	shift
+done
+
+
+#for the count of number of files in given directory
+		number=$( find $dir -type f | wc -l)
+		echo "Number of files in $dir are $number"
+
+if [ $number -gt 0 ]; then
+	echo "The $dir directory uses:"
+	du -sh $dir # command to calculate the size of the directory
+	echo "The $default largest Pictures in the ~/$dir directory are:"
+	cd $dir
+# command to find no of largest files in the directory
+	find . -type f -exec du -h {} \; | sort -h | tail -$default
+else
+	error-message " Bad Arguments"
+	echo "type -h or --help to get help"
+fi
--- bash/lab4/showinterface.sh
+++ bash/lab4/showinterface.sh
@@ -2,30 +2,84 @@
 # this script enumerates our 2 interfaces and
 # displays their configured IPV4 addresses
 # as well as default route information
+
+
 # Variables
 ###########
-showroute=0 # default is to not show the default route
-declare -A ips # hash of ip addresses keyed on interface name
-declare -a intfs # array of interface names supplied on the command line
-declare -a interfaces # array of discovered interface names
+Mainroute=0 # default is to not show the default route
+declare -A iphash # hash of ip addresses keyed on interface name
+declare -a int # array of interface names supplied on the command line
+declare -a intfc # array of discovered interface names
+
+#help_Function
+Help_Function(){
+	cat<<-EOF
+		This will print the ipv4 addresses
+		for the arguments in command line
+		And When interface name is not given
+		it will print information of all interfaces
+		
+		other arguents
+
+		filename --route or -r	#It will print Main route
+		filename interfacename	#it will print ipv4 address of provided interface
+	EOF
+}
+
+error-message () {
+        echo "error-message $filename: $1" >&2
+	echo "use -h or --help option to get help"
+}
+
 # Main
 ######
+
 # check the command line for an interface name or names, and -r|--route
 while [ $# -gt 0 ]; do
-case "$1" in
--r|--route )
-showroute=1 # set showroute to a 1 if asked to display default route
-;;
+	case "$1" in
+	-h| --help )
+		Help_Function
+		exit 0
+		;;
+
+	-r|--route )
+		Mainroute=1 # set showroute to a 1 if asked to display default route
+	;;
 *)
-intfs+=("$1") # add unnamed parameters as interface names
-;;
-esac
-shift
+		int+=("$1") # add unnamed parameters as interface names
+	;;
+	esac
+	shift
 done
+
+
 # Get an array of our interface names, we will have at least 2
-interfaces=(`ifconfig |grep '^[A-Za-z]'|awk '{print $1}'`)
+intfc=(`ifconfig |grep '^[A-Za-z]'|awk '{print $1}'`)
+
+
 # For each interface in the array, get the IP address and
 # save it to an array for IP addresses
-for intf in ${interfaces[@]}; do
-ips[$intf]=`ifconfig $intf|grep "inet "|sed -e 's/.*inet addr://' -e 's/ .*//'`
+for intf in ${intfc[@]}; do
+	iphash[$intf]=`ifconfig $intf|grep "inet "|sed -e 's/.*inet addr://' -e 's/ .*//'`
 done
+
+# Get default route gateway IP from the route command
+gwip=`route -n|grep '^0.0.0.0'|awk '{print $2}'`
+
+
+# Display the information we have gathered as requested
+if [ ${#int[@]} -gt 0 ]; then # use specified interface list if we have one
+	for intf in ${int[@]}; do # iterate over interface names requested
+		if [ ${iphash[$intf]} ]; then # only display info for an interface if we have some
+		echo "$intf has address ${iphash[$intf]}"
+	else # for invalid interface names or interfaces with no address, just let the user know about it
+		echo "$intf is not an interface on this host or has no ip address assigned"
+		fi
+	done
+else
+	for intf in ${intfc[@]}; do # if no interfaces specified, display them all
+		echo "$intf has address ${iphash[$intf]}"
+	done
+fi
+# display the default route gateway if we were given -r or --route on the command line
+[ $Mainroute -eq 1 ] && echo "The default route is through $gwip"
